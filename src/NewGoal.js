2cebf872cb30946aba3537f1eda59baf1ee5da52
Tobias Norton <tnorton20@yahoo.com>
2019-03-13 15:39:03 -0400

    NewGoal successfully creates new goal

--- src/NewGoal.js
+++ src/NewGoal.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react'
 import Header from './Header'
+import axios from 'axios'
 
 class NewGoal extends Component {
+  createGoal = event => {
+    event.preventDefault()
+    let form = event.target
+    let formData = new FormData(form)
+    for (let pair of formData.entries()) {
+      console.log(pair[0] + ', ' + pair[1])
+    }
+
+    axios.post('/api/goals', formData).then(response => {
+      form.reset()
+    })
+  }
+
   render() {
     return (
       <div>
@@ -15,22 +29,22 @@ class NewGoal extends Component {
 
         <section>
           <p className="header">Create a New Goal</p>
-          <form className="new-goal">
+          <form className="new-goal" onSubmit={this.createGoal}>
             <div className="input-container">
               <label>Goal Name</label>
-              <input type="text" />
+              <input type="text" name="goal[name]" />
             </div>
             <div className="input-container">
               <label>Amount</label>
-              <input type="number" />
+              <input type="number" name="goal[target_amount]" />
             </div>
             <div className="input-container">
               <label>Initial Deposit</label>
-              <input type="number" />
+              <input type="number" name="goal[balance]" />
             </div>
             <div className="input-container">
               <label>Deadline</label>
-              <input type="date" />
+              <input type="date" name="goal[due_date]" />
             </div>
             <button type="submit" className="submit">
               Submit
